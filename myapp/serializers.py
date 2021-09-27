f4b58afb9635e32230773af36242c39cdfd0b18a
Mr Vik <71473651+mrviktorv@users.noreply.github.com>
2021-09-27 22:40:56 +0300

    Update serializers.py

--- myapp/serializers.py
+++ myapp/serializers.py
@@ -1,11 +1,6 @@
 from rest_framework import serializers
 from myapp.models import Task
-# from myapp.models import Car
 
-# class CarSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model=Car
-#         fields=('name', 'top_speed')
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
