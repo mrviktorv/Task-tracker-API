9fc027f44355c435124f75a5e369efa2a62e8f10
Mr Vik <71473651+mrviktorv@users.noreply.github.com>
2021-09-27 22:40:43 +0300

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-# class Car(models.Model):
-#     name = models.CharField(max_length=100)
-#     top_speed = models.IntegerField()
-# Create your models here.
-
 class Task(models.Model):
     TaskId = models.AutoField(primary_key=True)
     Text = models.CharField(max_length=50)
